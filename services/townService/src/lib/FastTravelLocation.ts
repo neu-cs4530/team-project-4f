886985471c8cb21e1e0488d0563ebf8cf4bee886
Haicheng Liu <liu.haic@northeastern.edu>
2022-03-29 12:11:10 -0400

    task3 FastTravelLocation class, no methods

--- services/townService/src/lib/FastTravelLocation.ts
+++ services/townService/src/lib/FastTravelLocation.ts
@@ -1,3 +1,51 @@
 import CoveyTownController from './CoveyTownController';
 import { CoveyTownList } from '../CoveyTypes';
+import Player from '../types/Player';
+import PlayerSession from '../types/PlayerSession';
+import CoveyTownListener from '../types/CoveyTownListener';
+import { BoundingBox } from '../client/TownsServiceClient';
 
+/**
+ * The FastTravelLocation implements the logic for each fast travel location: managing the various events that
+ * can occur (e.g. generate a fast travel location(FTL), or delete a FTL)
+ */
+export default class FastTravelLocation {
+
+  get capacity(): number {
+    return this._capacity;
+  }
+
+  get players(): Player[] {
+    return this._players;
+  }
+
+  get FTLName(): string {
+    return this._FTLName;
+  }
+
+  get isFullyOccupied(): boolean {
+      return this._isFullyOccupied;
+  }
+
+  //   Prob need a list unaccessable of location?
+  //   get isValidLocation(): boolean {
+  //       return this._isValidLocation;
+  //   }
+
+
+  /** The list of players currently in the FTL * */
+  private _players: Player[] = [];
+
+  private _FTLName: string;
+
+  private _isFullyOccupied: boolean;
+
+  private _capacity: number;
+
+  constructor(FTLName: string) {
+    this._capacity = 10;            // some random magic number
+    this._isFullyOccupied = false;  // not fully occupied by default 
+    this._FTLName = FTLName;
+  }
+
+}
